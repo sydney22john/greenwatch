f16ba0ebccfbd0b7ea477ec8ed745e789ce358fe
Sydney John <sydney22john@gmail.com>
2024-05-18 22:21:09 -0600

    restructured and moved source directories so I can develop with golang as the backend

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func runServer(port string) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello from /test")
+	})
+
+	s := http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	// log.Fatalln(s.ListenAndServe())
+	_ = s
+}
